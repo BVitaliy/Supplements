e928f2fce5a0320cdbee6582a378a790a9b94c96
Virtual Pro <v.belza008@gmail.com>
2024-02-06 23:26:32 +0200

    update product detail

--- src/app/pages/product-detail/pages/add-review/add-review.page.ts
+++ src/app/pages/product-detail/pages/add-review/add-review.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { NavController } from '@ionic/angular';
 
 @Component({
@@ -7,11 +8,18 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./add-review.page.scss'],
 })
 export class AddReviewPage implements OnInit {
+  form!: FormGroup;
   product: any;
 
   constructor(public navCtrl: NavController) {}
 
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, [Validators.required]),
+      comment: new FormControl(null, [Validators.required]),
+      rate: new FormControl(null, [Validators.required]),
+    });
+
     this.product = {
       category: 'Athletic Greens',
       title:
