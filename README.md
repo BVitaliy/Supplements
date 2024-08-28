1efbbbbfb498167d27436b3d730f90c8c6c52ef5
Virtual Pro <v.belza008@gmail.com>
2024-08-28 21:55:49 +0300

    update readme

---
+++ README.md
@@ -0,0 +1,42 @@
+<a id="readme-top"></a>
+
+<h2 align="center">Supplement Score</h2>   
+    
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <a href="https://github.com/kram-nick/Supplement-Score">
+    <img src="src/assets/img/logo.svg" alt="Logo" width="80" height="80">
+  </a>
+</div>
+
+<!-- ABOUT THE PROJECT -->
+
+## About The Project
+
+Supplement Score is the fastest, simplest, and only way to get all the unbiased information you care about for any product with a click of a button, all in in one place. The larg Supplement Database, simplified, at your fingertips, like never before. That’s Supplement Snoop.
+
+Immediately view verified product reviews, third party test results, full ingredient breakdowns, and more just by scanning a product barcode or entering a product name. Take back control and be truly informed before making any supplement purchase.
+
+## Built With
+
+- [![Angular][Angular.io]][Angular-url]
+- [![Ionic Framework][Ionic.io]][Ionic-url]
+
+## Requirements
+
+- IOS 12+
+- Android 8+
+
+## Installation and Usage
+
+- Currently, the application is at the stage of testing in closed access.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- MARKDOWN LINKS & IMAGES -->
+
+[Ionic.io]: https://img.shields.io/badge/Ionic-Framework-3880FF?logo=ionic&logoColor=white
+[Ionic-url]: https://ionicframework.com/
+[Angular.io]: https://img.shields.io/badge/Angular-DD0031?style=for-the-badge&logo=angular&logoColor=white
+[Angular-url]: https://angular.io/
---
+++ src/app/core/services/push-notifications.service.ts
@@ -0,0 +1,153 @@
+import { Injectable } from '@angular/core';
+// // import { OneSignal } from '@ionic-native/onesignal/ngx';
+// import { OneSignal } from '@awesome-cordova-plugins/onesignal/ngx';
+import { Storage } from '@ionic/storage';
+import { Router } from '@angular/router';
+
+import { environment } from '../../../environments/environment';
+import { DEVICE_ID_STORAGE_NAME, VOIP_TOKEN_STORAGE_NAME } from 'src/app/app.config';
+
+
+import OneSignal from 'onesignal-cordova-plugin';
+import NotificationReceivedEvent from 'onesignal-cordova-plugin/dist/NotificationReceivedEvent';
+import { Calling } from 'calling';
+import { Platform } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PushNotificationsService {
+
+  constructor(
+    // private oneSignal: OneSignal,
+    private platform: Platform,
+    private storage: Storage,
+    private router: Router
+  ) { }
+
+  // setupPush() {
+  //   // I recommend to put these into your environment.ts
+  //   this.oneSignal.startInit(environment.oneSignalAppId, environment.googleFirebaseSenderId);
+
+  //   this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.Notification);
+
+  //   this.oneSignal.getIds().then((ids: any) => {
+  //     this.storage.set(DEVICE_ID_STORAGE_NAME, ids?.userId);
+  //   });
+
+  //   // Notifcation was received in general
+  //   // DONT DELETE!!!!
+  //   // this.oneSignal.handleNotificationReceived().subscribe(data => {
+  //   //   // const msg = data.payload.body;
+  //   //   // const title = data.payload.title;
+  //   //   // const additionalData = data.payload.additionalData;
+
+  //   //   console.log('handleNotificationReceived: ', data);
+  //   // });
+
+  //   // Notification was really clicked/opened
+  //   this.oneSignal.handleNotificationOpened().subscribe((data: any) => {
+  //     // const additionalData = data.notification.payload.additionalData;
+  //     console.log('handleNotificationOpened: ', data);
+  //     //
+  //     let url = data.notification.payload.launchURL;
+  //     // launchURL param starts with 'x:/' + route to necessary page
+  //     if (url && url.substring(0, 3) === 'x:/') {
+  //       url = url.substring(3);
+  //       // user clicks notification while the app is opened
+  //       if (data.notification.isAppInFocus) {
+  //         this.router.navigateByUrl(url);
+  //         // user clicks notification while the app is closed
+  //       } else {
+  //         function delay(ms: number) {
+  //           return new Promise(resolve => setTimeout(resolve, ms));
+  //         }
+  //         // firstly navigate to home page, if some necessary data for app to work correctly is loaded there
+  //         this.router.navigateByUrl('route-to-home-main-page').then(() => {
+  //           delay(1000).then(() => this.router.navigateByUrl(url));
+  //         });
+  //       }
+  //     }
+  //   });
+
+  //   // DONT DELETE!!!! In - App message
+  //   // this.oneSignal.handleInAppMessageClicked().subscribe(action => {
+  //   //     console.log('handleInAppMessageClicked: ', action);
+  //   //     let innerUrl = action.click_name;
+  //   //     if (innerUrl && innerUrl.substring(0, 7) === 'route:/') {
+  //   //         innerUrl = innerUrl.substring(7);
+  //   //         action.closes_message = true;
+  //   //         this.router.navigateByUrl(innerUrl);
+  //   //     }
+  //   // });
+
+  //   this.oneSignal.endInit();
+  // }
+
+  getIds() {
+    return new Promise((resolve, reject) => {
+      OneSignal.getDeviceState((response: any) => {
+        resolve(response);
+      })
+    });
+  }
+
+  setupPush() {
+    OneSignal.setAppId(environment.oneSignalAppId);
+
+    this.getIds().then((ids: any) => {
+      this.storage.set(DEVICE_ID_STORAGE_NAME, ids?.userId);
+    });
+
+    if (this.platform.is('ios')) {
+      Calling.getVoIPToken().then((data: any) => {
+        if (data?.token?.length) {
+          this.storage.set(VOIP_TOKEN_STORAGE_NAME, data?.token);
+        }
+      });
+    }
+
+    // Prompts the user for notification permissions.
+    //    * Since this shows a generic native prompt, we recommend instead using an In-App Message to prompt for notification permission (See step 7) to better communicate to your users what notifications they will get.
+    OneSignal.promptForPushNotificationsWithUserResponse(function(accepted) {
+      console.log("User accepted notifications: " + accepted);
+    });
+
+    OneSignal.setNotificationWillShowInForegroundHandler((notificationReceivedEvent: NotificationReceivedEvent) => {
+      console.log('setNotificationWillShowInForegroundHandler: ' + JSON.stringify(notificationReceivedEvent));
+      console.log('setNotificationWillShowInForegroundHandler1: ' + notificationReceivedEvent);
+      notificationReceivedEvent.complete(notificationReceivedEvent.getNotification());
+    });
+
+    OneSignal.setNotificationOpenedHandler((data) => {
+      console.log('notificationOpenedCallback: ' + JSON.stringify(data));
+      console.log('notificationOpenedCallback1: ' + data);
+      // console.log('notificationOpenedCallback: ' + data);
+      // let data = JSON.stringify(jsonData);
+      // let url = data.notification.launchURL;
+      let url: any = data.notification.additionalData;
+      // launchURL param starts with 'x:/' + route to necessary page
+      if (url?.app_data_url && url?.app_data_url.substring(0, 11) === 'https://x:/') {
+        url.app_data_url = url?.app_data_url.substring(11);
+        // user clicks notification while the app is opened
+        console.log('URL ', url?.app_data_url);
+
+
+        this.router.navigateByUrl(url?.app_data_url);
+        // if (data.notification.isAppInFocus) {
+        //   this.router.navigateByUrl(url);
+        //   // user clicks notification while the app is closed
+        // } else {
+        //   // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+        //   function delay(ms) {
+        //     return new Promise(resolve => setTimeout(resolve, ms));
+        //   }
+        //   // firstly navigate to home page, if some necessary data for app to work correctly is loaded there
+        //   this.router.navigateByUrl(url).then(() => {
+        //     delay(1000).then(() => this.router.navigateByUrl(url));
+        //   });
+        // }
+      }
+    });
+  }
+}
