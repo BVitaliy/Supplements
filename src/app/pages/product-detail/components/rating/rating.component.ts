04b57b978720b7fbf359949699be8831b9c92a51
Virtual Pro <v.belza008@gmail.com>
2024-09-24 10:59:06 +0300

    update

--- src/app/pages/product-detail/components/rating/rating.component.ts
+++ src/app/pages/product-detail/components/rating/rating.component.ts
@@ -6,9 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rating.component.scss'],
 })
 export class RatingComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
-
 }
